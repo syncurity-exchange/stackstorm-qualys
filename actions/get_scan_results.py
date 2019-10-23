07c2abe1b790f86873a040cfc32eb9fc88813a5a
JP Bourget <jp@syncurity.net>
2019-10-23 14:13:22 -0400

    get_scan_results

---
+++ actions/get_scan_results.py
@@ -0,0 +1,25 @@
+from lib.base import QualysBaseAction
+
+__all__ = [
+    'GetScanResults'
+]
+
+
+class LaunchScanAction(QualysBaseAction):
+    def run(self, scan_ref=None):
+        payload = {
+            'scan_ref': scan_ref,
+            'action': 'fetch'
+        }
+        scan_results = None
+        try:
+            scan_results = self.connection.request(
+                api_call='api/2.0/fo/scan',
+                data=payload,
+                api_version=2,
+                http_method='GET'
+            )
+            return True, scan_results
+        except ConnectionError as e:
+            return False, e
+
