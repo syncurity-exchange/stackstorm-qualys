b1d76f7e733671a2dda0a5fe2976ac519ba5c740
JP Bourget <jp@syncurity.net>
2019-10-24 16:51:37 -0400

    add json scan result output

--- actions/list_reports.py
+++ actions/list_reports.py
@@ -1,5 +1,8 @@
 from lib.base import QualysBaseAction
 
+from datetime import datetime
+import datetime
+
 __all__ = [
     'ListReportsAction'
 ]
@@ -8,4 +11,7 @@ __all__ = [
 class ListReportsAction(QualysBaseAction):
     def run(self):
         reports = self.connection.listReports()
+        output = self.resultsets.formatter(reports)
+        if isinstance(output['launch_datetime'], datetime.date):
+            output['launch_datetime'] = output['launch_datetime'].isoformat()
         return True, self.resultsets.formatter(reports)
